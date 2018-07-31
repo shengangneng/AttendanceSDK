b6e68a0ddc80702c124322607d9d203070dc5650
shengangneng <gangneng@hotmail.com>
2018-07-31 16:24:06 +0800

    导入项目相关文件

---
+++ MPMAttendance/BaseClass/MPMBaseTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MPMBaseTableViewCell.h
+//  MPMAtendence
+//
+//  Created by shengangneng on 2018/6/29.
+//  Copyright © 2018年 gangneng shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPMBaseTableViewCell : UITableViewCell
+
+@end
