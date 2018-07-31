9688777739bbb96e6b8a4ae9577960c5dc1965f7
shengangneng <gangneng@hotmail.com>
2018-07-31 16:05:11 +0800

    SDK

---
+++ MPMAttendance/MPMAttendance.h
@@ -0,0 +1,19 @@
+//
+//  MPMAttendance.h
+//  MPMAttendance
+//
+//  Created by shengangneng on 2018/7/31.
+//  Copyright © 2018年 MPMAttendanceSDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MPMAttendance.
+FOUNDATION_EXPORT double MPMAttendanceVersionNumber;
+
+//! Project version string for MPMAttendance.
+FOUNDATION_EXPORT const unsigned char MPMAttendanceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPMAttendance/PublicHeader.h>
+
+
